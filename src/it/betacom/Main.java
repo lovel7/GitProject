4a7906d185e6b0d02e4757a86f827af4bf26ff6e
lovel7 <134945126+lovel7@users.noreply.github.com>
2023-11-14 15:11:48 +0100

    Update Main.java

--- src/it/betacom/Main.java
+++ src/it/betacom/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		System.out.println("Benvenuto!");
-		System.out.println("Ciao modifica");
+		System.out.println("Ciao modifica7485");
 		System.out.println("Ciao 2");
 
 	}
