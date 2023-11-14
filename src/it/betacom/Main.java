8ecda2d5e3c9f7363758eae8934047909d9c69ea
EleonoraLovo <eleonora.lovo94@gmail.com>
2023-11-14 15:09:14 +0100

    COMMIT 2

--- src/it/betacom/Main.java
+++ src/it/betacom/Main.java
@@ -5,6 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		
 		System.out.println("Benvenuto!");
+		System.out.println("Ciao modifica");
+		System.out.println("Ciao 2");
 
 	}
 
